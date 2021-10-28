07f42203bb3ac3ed5c27d75796c8142caa5a3b09
Caio Sant'Ana da Silva <santanacaiio@gmail.com>
2021-10-28 13:30:23 -0300

    Adding new test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -13,11 +13,22 @@ RSpec.describe Project, type: :model do
 
     it 'should have a ability selected' do
         caio = ProjectOwner.create!({email: 'caio@gmail.com', password: '12345678'})
-        javascript = Ability.create!({name: 'Javascript'})
 
         projeto_caio = Project.new({title: 'Projeto de Sistema', description: 'Este projeto da Handa promete garantir qualidade de vida para muitos connect on Linked in. A Oestia, grupo que promove a intervenção da Handa no público, promoveu há cerca de três anos.',
         max_value: 200, deadline: 5.days.from_now, location: 'local', abilities: [], project_owner: caio})
 
         expect(projeto_caio.valid?).to eq false
+    end
+
+    
+    it 'isnt my project' do
+        caio = ProjectOwner.create!({email: 'caio@gmail.com', password: '12345678'})
+        bruno = ProjectOwner.create!({email: 'bruno@gmail.com', password: '12345678'})
+        javascript = Ability.create!({name: 'Javascript'})
+
+        projeto_caio = Project.new({title: 'Projeto de Sistema', description: 'Este projeto da Handa promete garantir qualidade de vida para muitos connect on Linked in. A Oestia, grupo que promove a intervenção da Handa no público, promoveu há cerca de três anos.',
+        max_value: 200, deadline: 5.days.from_now, location: 'local', abilities: [javascript], project_owner: bruno})
+
+        expect(Project.my_project?(projeto_caio, caio)).to eq false
     end
 end
